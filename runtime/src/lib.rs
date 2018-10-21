53d7d01b5b2681150dcda82292505543674d308b
Gav <gavin@parity.io>
2018-10-21 19:52:20 +0200

    Cleanups and update.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -7,50 +7,39 @@
 
 extern crate sr_std as rstd;
 extern crate sr_io as runtime_io;
-
 #[macro_use]
 extern crate sr_api as runtime_api;
-
 #[macro_use]
 extern crate srml_support;
-
 #[macro_use]
 extern crate sr_primitives as runtime_primitives;
-
 #[cfg(feature = "std")]
 #[macro_use]
 extern crate serde_derive;
-
 extern crate substrate_primitives as primitives;
-
 #[macro_use]
 extern crate parity_codec_derive;
-
-extern crate srml_balances as balances;
-extern crate srml_consensus as consensus;
-extern crate srml_contract as contract;
-extern crate srml_council as council;
-extern crate srml_democracy as democracy;
-extern crate srml_executive as executive;
-extern crate srml_session as session;
-extern crate srml_staking as staking;
-extern crate srml_system as system;
-extern crate srml_timestamp as timestamp;
-extern crate srml_treasury as treasury;
 #[macro_use]
 extern crate sr_version as version;
+extern crate srml_system as system;
+extern crate srml_executive as executive;
+extern crate srml_consensus as consensus;
+extern crate srml_timestamp as timestamp;
+extern crate srml_balances as balances;
 
 use rstd::prelude::*;
 #[cfg(feature = "std")]
 use primitives::bytes;
+use primitives::AuthorityId;
 use runtime_primitives::{ApplyResult, transaction_validity::TransactionValidity,
-	generic, traits::{self, BlakeTwo256, Block as BlockT}
+	Ed25519Signature, generic, traits::{self, BlakeTwo256, Block as BlockT}
 };
 use runtime_api::runtime::*;
 use version::{RuntimeVersion, ApiId};
 #[cfg(feature = "std")]
 use version::NativeVersion;
 
+// A few exports that help ease life for downstream crates.
 #[cfg(any(feature = "std", test))]
 pub use runtime_primitives::BuildStorage;
 pub use consensus::Call as ConsensusCall;
@@ -59,37 +48,18 @@ pub use balances::Call as BalancesCall;
 pub use runtime_primitives::{Permill, Perbill};
 pub use timestamp::BlockPeriod;
 pub use srml_support::StorageValue;
-#[cfg(any(feature = "std", test))]
 
-/// An index to a block.
-pub type BlockNumber = u64;
-
-/// Alias to Ed25519 pubkey that identifies an account on the chain. This will almost
-/// certainly continue to be the same as the substrate's `AuthorityId`.
+/// Alias to Ed25519 pubkey that identifies an account on the chain.
 pub type AccountId = primitives::H256;
-
-/// The type for looking up accounts. We don't expect more than 4 billion of them, but you
-/// never know...
-pub type AccountIndex = u32;
-
-/// Balance of an account.
-pub type Balance = u128;
-
-/// The Ed25519 pub key of an session that belongs to an authority of the chain. This is
-/// exactly equivalent to what the substrate calls an "authority".
-pub type SessionKey = primitives::AuthorityId;
-
-/// Index of a transaction in the chain.
-pub type Index = u64;
 
 /// A hash of some data used by the chain.
 pub type Hash = primitives::H256;
 
-/// Alias to 512-bit hash when used in the context of a signature on the chain.
-pub type Signature = runtime_primitives::Ed25519Signature;
+/// Index of a block number in the chain.
+pub type BlockNumber = u64;
 
-/// A timestamp: seconds since the unix epoch.
-pub type Moment = u64;
+/// Index of an account's extrinsic in the chain.
+pub type Nonce = u64;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -107,21 +77,19 @@ pub mod opaque {
 			None
 		}
 	}
-	/// Header type.
-	pub type Header = generic::Header<BlockNumber, BlakeTwo256, generic::DigestItem<Hash, SessionKey>>;
-	/// Block type.
+	/// Opaque block header type.
+	pub type Header = generic::Header<BlockNumber, BlakeTwo256, generic::DigestItem<Hash, AuthorityId>>;
+	/// Opaque block type.
 	pub type Block = generic::Block<Header, UncheckedExtrinsic>;
-	/// Block ID.
+	/// Opaque block identifier type.
 	pub type BlockId = generic::BlockId<Block>;
 }
 
-// Appears to be broken on current nightly.
 const BLOCK_BUILDER: ApiId = *b"blkbuild";
 const TAGGED_TRANSACTION_QUEUE: ApiId = *b"validatx";
 const METADATA: ApiId = *b"metadata";
 
-/// Runtime version.
-#[cfg(feature = "std")]
+/// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: ver_str!("template-node"),
 	impl_name: ver_str!("substrate-template-node"),
@@ -136,23 +104,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	]),
 };
 
-/// Runtime version.
-#[cfg(not(feature = "std"))]
-pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: ver_str!("template-node"),
-	impl_name: ver_str!("substrate-template-node"),
-	authoring_version: 1,
-	spec_version: 1,
-	impl_version: 0,
-	apis: apis_vec!([
-		// compiles on nightly, but breaks on stable due to lifetime issues.
-		(*b"blkbuild", 1),
-		(TAGGED_TRANSACTION_QUEUE, 1),
-		(METADATA, 1)
-	]),
-};
-
-/// Native version.
+/// The version infromation used to identify this runtime when compiled natively.
 #[cfg(feature = "std")]
 pub fn native_version() -> NativeVersion {
 	NativeVersion {
@@ -162,40 +114,62 @@ pub fn native_version() -> NativeVersion {
 }
 
 impl system::Trait for Runtime {
-	type Origin = Origin;
-	type Index = Index;
-	type BlockNumber = BlockNumber;
-	type Hash = Hash;
-	type Hashing = BlakeTwo256;
-	type Digest = generic::Digest<Log>;
+	/// The identifier used to distinguish between accounts.
 	type AccountId = AccountId;
+	/// The index type for storing how many extrinsics an account has signed.
+	type Index = Nonce;
+	/// The index type for blocks.
+	type BlockNumber = BlockNumber;
+	/// The type for hashing blocks and tries.
+	type Hash = Hash;
+	/// The hashing algorithm used.
+	type Hashing = BlakeTwo256;
+	/// The header digest type.
+	type Digest = generic::Digest<Log>;
+	/// The header type.
 	type Header = generic::Header<BlockNumber, BlakeTwo256, Log>;
+	/// The ubiquitous event type.
 	type Event = Event;
+	/// The ubiquitous log type.
 	type Log = Log;
+	/// The ubiquitous origin type.
+	type Origin = Origin;
 }
 
 impl consensus::Trait for Runtime {
+	/// The position in the block's extrinsics that the note-offline inherent must be placed.
 	const NOTE_OFFLINE_POSITION: u32 = 1;
-	type Log = Log;
-	type SessionKey = SessionKey;
+	/// The identifier we use to refer to authorities.
+	type SessionKey = AuthorityId;
+	/// No action in case an authority was determined to be offline.
 	type OnOfflineValidator = ();
+	/// The ubiquitous log type.
+	type Log = Log;
 }
 
 impl timestamp::Trait for Runtime {
+	/// The position in the block's extrinsics that the timestamp-set inherent must be placed.
 	const TIMESTAMP_SET_POSITION: u32 = 0;
-	type Moment = Moment;
+	/// A timestamp: seconds since the unix epoch.
+	type Moment = u64;
 }
 
 impl balances::Trait for Runtime {
-	type Balance = Balance;
-	type AccountIndex = AccountIndex;
+	/// The type for recording an account's balance.
+	type Balance = u128;
+	/// The type for recording indexing into the account enumeration. If this ever overflows, there
+	/// will be problems!
+	type AccountIndex = u32;
+	/// What to do if an account's free balance gets zeroed.
 	type OnFreeBalanceZero = ();
+	/// Restrict whether an account can transfer funds. We don't place any further restrictions.
 	type EnsureAccountLiquid = ();
+	/// The uniquitous event type.
 	type Event = Event;
 }
 
 construct_runtime!(
-	pub enum Runtime with Log(InternalLog: DigestItem<Hash, SessionKey>) where
+	pub enum Runtime with Log(InternalLog: DigestItem<Hash, AuthorityId>) where
 		Block = Block,
 		UncheckedExtrinsic = UncheckedExtrinsic
 	{
@@ -216,19 +190,20 @@ pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 /// BlockId type as expected by this runtime.
 pub type BlockId = generic::BlockId<Block>;
 /// Unchecked extrinsic type as expected by this runtime.
-pub type UncheckedExtrinsic = generic::UncheckedMortalExtrinsic<Address, Index, Call, Signature>;
+pub type UncheckedExtrinsic = generic::UncheckedMortalExtrinsic<Address, Nonce, Call, Ed25519Signature>;
 /// Extrinsic type that has already been checked.
-pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Index, Call>;
+pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Nonce, Call>;
 /// Executive: handles dispatch to the various modules.
 pub type Executive = executive::Executive<Runtime, Block, Context, Balances, AllModules>;
 
+// Implement our runtime API endpoints. This is just a bunch of proxying.
 impl_apis! {
-	impl Core<Block, SessionKey> for Runtime {
+	impl Core<Block, AuthorityId> for Runtime {
 		fn version() -> RuntimeVersion {
 			VERSION
 		}
 
-		fn authorities() -> Vec<SessionKey> {
+		fn authorities() -> Vec<AuthorityId> {
 			Consensus::authorities()
 		}
 
