f59bddeca5e4bb78775456c8266a76adf839cb29
Alexander Krupenkin <mail@akru.me>
2019-03-19 11:16:03 +0300

    BSD3 -> APACHE2

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,3 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright 2018-2019 Airalab <research@aira.life> 
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
 //! The Robonomics runtime. This can be compiled with `#[no_std]`, ready for Wasm.
 
 #![cfg_attr(not(feature = "std"), no_std)]
