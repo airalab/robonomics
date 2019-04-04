118f24630a42b81c3580f0a2d722ee739b387c0f
Alexander Krupenkin <mail@akru.me>
2019-04-04 11:00:54 +0300

    Update to substrate v1.0

--- src/cli/chain_spec.rs
+++ src/cli/chain_spec.rs
@@ -114,13 +114,16 @@ pub fn testnet_genesis(
     const SECS_PER_BLOCK: u64 = 4;
     const MINUTES: u64 = 60 / SECS_PER_BLOCK;
 
-    const ENDOWMENT: u128 = 10_000_000 * XRT;
-    const STASH: u128 = 100 * XRT;
+    const ENDOWMENT: u128 = 100 * XRT;
+    const STASH: u128 = 1_000 * XRT;
 
     GenesisConfig {
         consensus: Some(ConsensusConfig {
             code: include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/robonomics_runtime.compact.wasm").to_vec(),
-            authorities: initial_authorities.iter().map(|x| x.2.clone()).collect(),
+            authorities: initial_authorities
+                .iter()
+                .map(|x| x.2.clone())
+                .collect(),
         }),
         system: None,
         indices: Some(IndicesConfig {
@@ -132,30 +135,42 @@ pub fn testnet_genesis(
             transaction_byte_fee: 50 * COASE,
             transfer_fee: 0,
             creation_fee: 0,
-            balances: endowed_accounts.iter()
+            balances: endowed_accounts
+                .iter()
                 .map(|k| (k.clone(), ENDOWMENT))
                 .chain(initial_authorities.iter().map(|x| (x.0.clone(), STASH)))
                 .collect(),
             vesting: vec![],
         }),
         session: Some(SessionConfig {
-            validators: initial_authorities.iter().map(|x| x.1.clone()).collect(),
+            validators: initial_authorities
+                .iter()
+                .map(|x| x.1.clone())
+                .collect(),
             session_length: 15,
-            keys: initial_authorities.iter().map(|x| (x.1.clone(), x.2.clone())).collect::<Vec<_>>(),
+            keys: initial_authorities
+                .iter()
+                .map(|x| (x.1.clone(), x.2.clone()))
+                .collect::<Vec<_>>(),
         }),
         staking: Some(StakingConfig {
             current_era: 0,
             minimum_validator_count: 2,
             validator_count: 7,
-            sessions_per_era: 60,
-            bonding_duration: 60 * MINUTES,
+            sessions_per_era: 10,
+            bonding_duration: 10 * MINUTES,
             current_session_reward: 0,
             session_reward: Perbill::from_millionths(200_000),
             offline_slash: Perbill::from_millionths(1_000_000),
             offline_slash_grace: 4,
-            stakers: initial_authorities.iter()
-                .map(|x| (x.0.clone(), x.1.clone(), STASH, StakerStatus::Validator)).collect(),
-            invulnerables: initial_authorities.iter().map(|x| x.1.clone()).collect(),
+            stakers: initial_authorities
+                .iter()
+                .map(|x| (x.0.clone(), x.1.clone(), STASH, StakerStatus::Validator))
+                .collect(),
+            invulnerables: initial_authorities
+                .iter()
+                .map(|x| x.1.clone())
+                .collect(),
         }),
         timestamp: Some(TimestampConfig {
             minimum_period: SECS_PER_BLOCK / 2,
@@ -164,7 +179,10 @@ pub fn testnet_genesis(
             key: root_key,
         }),
         grandpa: Some(GrandpaConfig {
-            authorities: initial_authorities.iter().map(|x| (x.2.clone(), 1)).collect(),
+            authorities: initial_authorities
+                .iter()
+                .map(|x| (x.2.clone(), 1))
+                .collect(),
         }),
     }
 }
@@ -177,32 +195,27 @@ fn xrt_props() -> Properties {
 /// Robonomics testnet config. 
 fn robonomics_config_genesis() -> GenesisConfig {
     let aira_auth: AuthorityId  = hex!["30d3114363ff180bb295099c34fb30060e3b2df89617f7d76078b37d4d351cca"].unchecked_into();
-    let aira_stash: AccountId   = hex!["4e431ea08693fcba3466628c740703a3244308570646765dc388642c77b6bc28"].unchecked_into();
-    let aira_control: AccountId = hex!["3ae9a59c2c2a0bf06d9a58a47eaef86a6ce8eee356eb5d29574739d0d0c33d30"].unchecked_into();
+    let aira_stash: AccountId   = hex!["0ab623ec23b0346976d8fb4eaf012035fda269077490cbfb6aae15cb31d43777"].unchecked_into();
+    let aira_control: AccountId = hex!["16e4b93d965a27e50de7e27b6e9b8471186b4a463bbad8e2b0a398007098504e"].unchecked_into();
 
     let akru_auth: AuthorityId  = hex!["4327b538c4d3fd84cb875328adeee97ee0754dc1491c5a453c07031a40215b0e"].unchecked_into();
-    let akru_stash: AccountId   = hex!["9656aa462eb8b0ba6ddcb46a7001bacda12abe826173b7db8a33bc1a612cc253"].unchecked_into(); 
-    let akru_control: AccountId = hex!["0ae0b0afc9783c4e7493e56d9572fc45c024e12bbef7d3abf2534c2d07acb81b"].unchecked_into(); 
-
-    let khssnv_auth: AuthorityId  = hex!["0b703dcc8367d3b98bfaead0c362a89a3722c9756919150f3727de07080b5f1e"].unchecked_into();
-    let khssnv_stash: AccountId   = hex!["2ea77f95a8a9462c11b1dd65bea115631c8538c9f4b3998a44f754a9e2a46556"].unchecked_into();
-    let khssnv_control: AccountId = hex!["78d4af7d1b359f0648e21f51c429bde663886706eda868815931984414e8f667"].unchecked_into();
+    let akru_stash: AccountId   = hex!["a26253010447e4a0ec7ddce034034a4ebcfb1317440fd458c21c592ddf8d0337"].unchecked_into();
+    let akru_control: AccountId = hex!["16eb796bee0c857db3d646ee7070252707aec0c7d82b2eda856632f6a2306a58"].unchecked_into();
 
     testnet_genesis(
         vec![
           (aira_stash, aira_control.clone(), aira_auth),
           (akru_stash, akru_control.clone(), akru_auth),
-          (khssnv_stash, khssnv_control.clone(), khssnv_auth),
         ],
         akru_control.clone(),
-        Some(vec![aira_control, akru_control, khssnv_control]),
+        Some(vec![aira_control, akru_control]),
     )
 }
 
 /*
 /// Robonomics testnet config. 
-pub fn robonomics_testnet_config() -> Result<ChainSpec, String> {
-    ChainSpec::from_embedded(include_bytes!("../res/robonomics.json"))
+pub fn robonomics_testnet_config() -> ChainSpec {
+    ChainSpec::from_embedded(include_bytes!("../../res/robonomics_testnet.json")).unwrap()
 }
 */
 
