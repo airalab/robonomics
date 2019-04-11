825cd87575b827f2501b42681588d65f56c2871f
Alexander Krupenkin <mail@akru.me>
2019-04-11 22:51:51 +0300

    Upgrade to latest substrate
    * Upgrade to GRANDPA v0.7

--- shell.nix
+++ shell.nix
@@ -6,14 +6,14 @@ with nixpkgs.latest.rustChannels;
 with nixpkgs;
 
 let
-  channel = rustChannelOf { date = "2019-04-02"; channel = "nightly"; };
+  channel = rustChannelOf { date = "2019-04-11"; channel = "nightly"; };
   rust = channel.rust.override { targets = [ "wasm32-unknown-unknown" ]; };
   msgs = callPackage ./robonomics_msgs { };
 in
   stdenv.mkDerivation {
     name = "substrate-nix-shell";
     buildInputs = [
-      rust wasm-gc pkgconfig openssl clang msgs
+      rust msgs wasm-gc pkgconfig openssl clang
     ];
     LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
   }
