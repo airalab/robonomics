28b25055d91d54f01813937003f4144750b42650
strdn <eilyichev@aira.life>
2019-07-31 13:32:22 +0300

    fix turtlesim example

--- release.nix
+++ release.nix
@@ -9,5 +9,6 @@ let
 in rec {
   rustWasm = channel.rust.override { targets = [ "wasm32-unknown-unknown" ]; };
   msgs = callPackage ./substrate-ros/msgs/substrate_ros_msgs { };
+  turtlesim = callPackage ./examples/turtlesim_liability { inherit msgs; };
   node = callPackage ./. { inherit rustWasm msgs; };
 }
