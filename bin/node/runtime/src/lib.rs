e678aa1ed7ffe240d2f13cf3fd30b3f85f022985
Alexander Krupenkin <mail@akru.me>
2019-11-18 10:26:20 +0300

    Bump version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -75,7 +75,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("robonomics"),
     impl_name: create_runtime_str!("robonomics-airalab"),
     authoring_version: 1,
-    spec_version: 40,
+    spec_version: 50,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
 };
