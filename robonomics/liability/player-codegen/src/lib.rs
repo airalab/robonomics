22286fe50f8f5d715ec6f978c1f040723616e00e
Alexander Krupenkin <mail@akru.me>
2020-02-26 05:41:54 +0300

    Bump copyright years

--- robonomics/liability/player-codegen/src/lib.rs
+++ robonomics/liability/player-codegen/src/lib.rs
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright 2018-2019 Airalab <research@aira.life>
+//  Copyright 2018-2020 Airalab <research@aira.life>
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
