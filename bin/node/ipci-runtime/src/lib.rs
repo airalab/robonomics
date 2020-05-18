82b7146508a5435078cdf165f7c547ff3bcdffff
Alexander Krupenkin <mail@akru.me>
2020-05-18 13:09:59 +0300

    Bump version

--- bin/node/ipci-runtime/src/lib.rs
+++ bin/node/ipci-runtime/src/lib.rs
@@ -74,8 +74,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to equal spec_version. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 5,
-    impl_version: 5,
+    spec_version: 6,
+    impl_version: 6,
     apis: RUNTIME_API_VERSIONS,
 };
 
