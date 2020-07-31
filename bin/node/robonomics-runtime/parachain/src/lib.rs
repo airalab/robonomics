23858a9e780ccda54bedae5fb47147aca5cb820c
Alexander Krupenkin <mail@akru.me>
2020-07-31 08:33:20 +0300

    Fix code style

--- bin/node/robonomics-runtime/parachain/src/lib.rs
+++ bin/node/robonomics-runtime/parachain/src/lib.rs
@@ -450,7 +450,7 @@ construct_runtime! {
         Balances: pallet_balances::{Module, Call, Storage, Event<T>, Config<T>},
         GenericAsset: pallet_generic_asset::{Module, Call, Storage, Event<T>, Config<T>},
         TransactionPayment: pallet_transaction_payment::{Module, Storage},
-		Multisig: pallet_multisig::{Module, Call, Storage, Event<T>},
+        Multisig: pallet_multisig::{Module, Call, Storage, Event<T>},
 
         // Randomness.
         RandomnessCollectiveFlip: pallet_randomness_collective_flip::{Module, Call, Storage},
