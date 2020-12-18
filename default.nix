5dace3bb7ef03921ebb6473a68416a766014c73b
Alexander Krupenkin <mail@akru.me>
2020-12-18 14:43:04 +0300

    [CI] decrease verbosity (too long logs)

--- default.nix
+++ default.nix
@@ -15,9 +15,16 @@ rustPlatform.buildRustPackage {
   name = "robonomics";
   src = builtins.path { path = ./.; name = "robonomics-src"; };
   cargoSha256 = null; 
+
   propagatedBuildInputs = [ substrate-ros-msgs ];
   nativeBuildInputs = [ clang ];
   buildInputs = [ rust-nightly ];
+
+  # NOTE: We don't build the WASM runtimes since this would require a more
+  # complicated rust environment setup. The resulting binary is still useful for
+  # live networks since those just use the WASM blob from the network chainspec.
+  BUILD_DUMMY_WASM_BINARY = 1;
+
   ROCKSDB_LIB_DIR = "${rocksdb}/lib";
   LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
   PROTOC = "${protobuf}/bin/protoc";
