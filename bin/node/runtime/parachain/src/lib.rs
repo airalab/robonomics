fcb248a5b0dfad65fe8e31162aff2d37f0575051
Alexander Krupenkin <mail@akru.me>
2021-02-20 07:54:22 +0300

    Bump version, update rococo spec

--- bin/node/runtime/parachain/src/lib.rs
+++ bin/node/runtime/parachain/src/lib.rs
@@ -90,8 +90,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("robonomics"),
     impl_name: create_runtime_str!("robonomics-airalab"),
     authoring_version: 1,
-    spec_version: 3,
-    impl_version: 3,
+    spec_version: 4,
+    impl_version: 4,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
 };
