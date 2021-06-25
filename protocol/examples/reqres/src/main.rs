58faa50c726b009bfa2d2c59a57890fdfc412b29
Andriy  Mukha <admu@MobicaPL.local>
2021-06-25 15:35:32 +0300

    changed struct Req Resp to Enum, added bitcode (de)serialization

--- protocol/examples/reqres/src/main.rs
+++ protocol/examples/reqres/src/main.rs
@@ -1,4 +1,5 @@
 /// Example of usage simple request response protocol from reqresp crate.
+use bincode;
 use libp2p::core::{
     identity,
     muxing::StreamMuxerBox,
@@ -9,7 +10,6 @@ use libp2p::noise::{Keypair, NoiseConfig, X25519Spec};
 use libp2p::request_response::*;
 use libp2p::swarm::{Swarm, SwarmEvent};
 use libp2p::tcp::TcpConfig;
-use libp2p::yamux::YamuxConfig;
 use rust_base58::FromBase58;
 use std::env;
 use std::iter;
@@ -42,32 +42,42 @@ fn main() {
             match swarm1.next_event().await {
                 SwarmEvent::NewListenAddr(addr) => {
                     println!("Peer 1 listening on {}", addr.clone());
-                }
+                },
 
                 SwarmEvent::Behaviour(RequestResponseEvent::Message {
                     peer,
-                    message:
-                        RequestResponseMessage::Request {
-                            request, channel, ..
-                        },
+                    message: RequestResponseMessage::Request { request, channel, .. }
                 }) => {
-                    let req = String::from_utf8_lossy(&request.0);
-                    println!(" peer1 Req -> {:?} to {:?}", req.clone(), peer);
 
-                    if req == "quit" {
-                        thread::sleep(ms);
-                        process::exit(0x0100);
+                    // match type of request: Ping or Get and handle
+                    match request {
+                        Req::Get(data) =>  {
+                            //decode received request
+                            let decoded : Vec<u8> = bincode::deserialize(&data.to_vec()).unwrap();
+                            println!(" peer1 Get '{}' from  {:?}", String::from_utf8_lossy(&decoded[..]), peer);
+                            let resp : Resp = Resp::Data(format!("Hello {}", String::from_utf8_lossy(&decoded[..])).into_bytes());
+                            if let Resp::Data(y) = resp.clone() {
+                                println!(" peer1 Resp::Data '{}' to {:?}", String::from_utf8_lossy(&y), peer);
+                            }
+                            // send encoded response
+                            let resp_encoded: Vec<u8> = bincode::serialize(&format!("Hello {}", String::from_utf8_lossy(&decoded[..])).into_bytes()).unwrap();
+                            swarm1.behaviour_mut().send_response(channel, Resp::Data(resp_encoded)).unwrap();
+                        },
+
+                        Req::Ping() =>  {
+                            println!(" peer1 {:?} from {:?}", request, peer);
+                            let resp: Resp = Resp::Pong();
+                            println!(" peer1 {:?} to   {:?}", resp, peer);
+                            swarm1.behaviour_mut().send_response(channel, resp.clone()).unwrap();
+                        },
                     }
-                    let resp = Resp(format!("Hello {}", req).into_bytes());
-                    println!(" peer1 Resp : {:?}", String::from_utf8_lossy(&resp.0));
-                    swarm1
-                        .behaviour_mut()
-                        .send_response(channel, resp.clone())
-                        .unwrap();
-                }
 
-                SwarmEvent::Behaviour(RequestResponseEvent::ResponseSent { peer, .. }) => {
-                    println!("Response sent to peer ID {:?}", peer);
+                },
+
+                SwarmEvent::Behaviour(RequestResponseEvent::ResponseSent {
+                    peer, ..
+                }) => {
+                    println!("Response sent to {:?}",  peer);
                 }
 
                 SwarmEvent::Behaviour(e) => panic!("Peer1: Unexpected event: {:?}", e),
@@ -76,69 +86,58 @@ fn main() {
         }
     };
 
-    //  client part
+    //  client part 
     let peer2 = async move {
+
         let protocols = iter::once((ReqRespProtocol(), ProtocolSupport::Full));
         let cfg = RequestResponseConfig::default();
 
         let peer_id = std::env::args().nth(2).unwrap();
-        let remote_bytes = peer_id.from_base58().unwrap();
+        let remote_bytes = peer_id.from_base58().unwrap(); 
         let remote_peer = PeerId::from_bytes(&remote_bytes).unwrap();
 
         let (peer2_id, trans) = mk_transport();
         let ping_proto2 = RequestResponse::new(ReqRespCodec(), protocols, cfg);
         let mut swarm2 = Swarm::new(trans, ping_proto2, peer2_id.clone());
         println!("Local peer 2 id: {:?}", peer2_id);
-
+ 
         let addr_remote = std::env::args().nth(1).unwrap(); // remote  i.e. "/ip4/192.168.1.6/tcp/61241"
-        let addr_r: Multiaddr = addr_remote.parse().unwrap();
-        swarm2
-            .behaviour_mut()
-            .add_address(&remote_peer, addr_r.clone());
+        let addr_r : Multiaddr = addr_remote.parse().unwrap();
+        swarm2.behaviour_mut().add_address(&remote_peer, addr_r.clone());
 
-        let mut count = 0;
-        let mut rq = Req(count.to_string().into_bytes());
-        let mut req_id = swarm2
-            .behaviour_mut()
-            .send_request(&remote_peer, rq.clone());
-        println!(
-            " peer2 Req  ID {} -> {:?} : {}",
-            req_id,
-            remote_peer,
-            String::from_utf8_lossy(&rq.0)
-        );
+        let mut count : i64 = 0;
+        let mut rq = Req::Ping();
+        let mut req_id = swarm2.behaviour_mut().send_request(&remote_peer, rq.clone());
+        println!(" peer2 Req{}: Ping  -> {:?} : {:?}", req_id, remote_peer, rq);
 
         loop {
             match swarm2.next().await {
                 RequestResponseEvent::Message {
                     peer,
-                    message:
-                        RequestResponseMessage::Response {
-                            request_id,
-                            response,
-                        },
+                    message: RequestResponseMessage::Response { request_id, response }
                 } => {
+                    match response {
+                        Resp::Pong () => {
+                            println!(" peer2 Resp{} {:?} from {:?}", request_id, &response, peer);
+                        },
+                        Resp::Data (data) => {
+                            // decode response 
+                            let decoded : Vec<u8> = bincode::deserialize(&data.to_vec()).unwrap();
+                            println!(" peer2 Resp{}: Data '{}' from {:?}", req_id, String::from_utf8_lossy(&decoded[..]), remote_peer);
+                        }
+                    }
+                    rq = Req::Get(count.to_string().into_bytes());
+                    // send encoded request 
+                    if let Req::Get(y) = rq {
+                        println!(" peer2  Req{}: Get -> {:?} : '{}'", req_id, remote_peer, String::from_utf8_lossy(&y));
+                    }
+                    let req_encoded: Vec<u8> = bincode::serialize(&format!("{}", count).into_bytes()).unwrap();
+                    req_id = swarm2.behaviour_mut().send_request(&remote_peer, Req::Get(req_encoded));
                     count += 1;
-                    println!(
-                        " peer2 Resp ID {}  : {} to {:?}",
-                        request_id,
-                        String::from_utf8_lossy(&response.0),
-                        peer
-                    );
-                    rq = Req(count.to_string().into_bytes());
-                    req_id = swarm2
-                        .behaviour_mut()
-                        .send_request(&remote_peer, rq.clone());
-                    println!(
-                        " peer2 Req  ID {} -> {:?} : {}",
-                        req_id,
-                        remote_peer,
-                        String::from_utf8_lossy(&rq.0)
-                    );
                     thread::sleep(ms);
-                }
+                },
 
-                e => {
+                e =>  {
                     println!("Peer2 err: {:?}", e);
                     process::exit(0x0100)
                 }
@@ -146,7 +145,7 @@ fn main() {
         }
     };
 
-    if args.len() < 3 {
+    if  args.len() < 3 {
         let () = async_std::task::block_on(peer1);
     } else {
         let () = async_std::task::block_on(peer2);
@@ -154,31 +153,27 @@ fn main() {
 }
 
 fn mk_transport() -> (PeerId, transport::Boxed<(PeerId, StreamMuxerBox)>) {
-    // if provided pk8 file with keys use it to have static PeerID
+
+    // if provided pk8 file with keys use it to have static PeerID 
     // in other case PeerID  will be randomly generated
     let mut id_keys = identity::Keypair::generate_ed25519();
     let mut peer_id = id_keys.public().into_peer_id();
 
     let f = std::fs::read("private.pk8");
     let _ = match f {
-        Ok(mut bytes) => {
-            id_keys = identity::Keypair::rsa_from_pkcs8(&mut bytes).unwrap();
-            peer_id = id_keys.public().into_peer_id();
-            println!("try get peer ID from keypair at file");
-        }
-        Err(_e) => println!("try to use peer ID from random keypair"),
+        Ok(mut bytes) =>  {
+        id_keys = identity::Keypair::rsa_from_pkcs8(&mut bytes).unwrap();
+        peer_id = id_keys.public().into_peer_id();
+        println!("try get peer ID from keypair at file");
+       },
+        Err(_e) =>  println!("try to use peer ID from random keypair"),
     };
 
-    let noise_keys = Keypair::<X25519Spec>::new()
-        .into_authentic(&id_keys)
-        .unwrap();
-    (
-        peer_id,
-        TcpConfig::new()
-            .nodelay(true)
-            .upgrade(upgrade::Version::V1)
-            .authenticate(NoiseConfig::xx(noise_keys).into_authenticated())
-            .multiplex(YamuxConfig::default())
-            .boxed(),
-    )
+    let noise_keys = Keypair::<X25519Spec>::new().into_authentic(&id_keys).unwrap();
+    (peer_id, TcpConfig::new()
+        .nodelay(true)
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(noise_keys).into_authenticated())
+        .multiplex(libp2p_yamux::YamuxConfig::default())
+        .boxed())
 }
--- protocol/src/reqres.rs
+++ protocol/src/reqres.rs
@@ -25,11 +25,16 @@ use libp2p::request_response::RequestResponseCodec;
 use std::io;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Req(pub Vec<u8>);
+pub enum Req {
+    Ping (),
+    Get (Vec<u8>),
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Resp(pub Vec<u8>);
-
+pub enum Resp {
+    Pong (),
+    Data (Vec<u8>),
+}
 impl ProtocolName for ReqRespProtocol {
     fn protocol_name(&self) -> &[u8] {
         "/robonomics/1".as_bytes()
@@ -48,61 +53,50 @@ impl RequestResponseCodec for ReqRespCodec {
     type Request = Req;
     type Response = Resp;
 
-    async fn read_request<T>(
-        &mut self,
-        _: &ReqRespProtocol,
-        io: &mut T,
-    ) -> io::Result<Self::Request>
+    async fn read_request<T>(&mut self, _: &ReqRespProtocol, io: &mut T)
+        -> io::Result<Self::Request>
     where
-        T: AsyncRead + Unpin + Send,
+        T: AsyncRead + Unpin + Send
     {
         read_one(io, 1024)
             .map(|res| match res {
                 Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
-                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
-                Ok(vec) => Ok(Req(vec)),
+                Ok(vec) if vec.is_empty() => Ok(Req::Ping()),
+                Ok(vec) => Ok(Req::Get(vec)),
             })
             .await
     }
 
-    async fn read_response<T>(
-        &mut self,
-        _: &ReqRespProtocol,
-        io: &mut T,
-    ) -> io::Result<Self::Response>
+    async fn read_response<T>(&mut self, _: &ReqRespProtocol, io: &mut T)
+        -> io::Result<Self::Response>
     where
-        T: AsyncRead + Unpin + Send,
-    {
+        T: AsyncRead + Unpin + Send {
         read_one(io, 1024)
             .map(|res| match res {
                 Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
-                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
-                Ok(vec) => Ok(Resp(vec)),
+                Ok(vec) if vec.is_empty() => Ok(Resp::Pong()),
+                Ok(vec) => Ok(Resp::Data(vec)),
             })
             .await
     }
 
-    async fn write_request<T>(
-        &mut self,
-        _: &ReqRespProtocol,
-        io: &mut T,
-        Req(data): Req,
-    ) -> io::Result<()>
+    async fn write_request<T>(&mut self, _: &ReqRespProtocol, io: &mut T, req : Req )
+        -> io::Result<()>
     where
-        T: AsyncWrite + Unpin + Send,
-    {
-        write_one(io, data).await
+        T: AsyncWrite + Unpin + Send {
+        match req {
+         Req::Ping() =>  write_one(io, "".as_bytes()).await,
+         Req::Get(data) =>  write_one(io, data).await,
+       }
     }
 
-    async fn write_response<T>(
-        &mut self,
-        _: &ReqRespProtocol,
-        io: &mut T,
-        Resp(data): Resp,
-    ) -> io::Result<()>
+    async fn write_response<T>(&mut self, _: &ReqRespProtocol, io: &mut T, resp: Resp)
+        -> io::Result<()>
     where
-        T: AsyncWrite + Unpin + Send,
-    {
-        write_one(io, data).await
+        T: AsyncWrite + Unpin + Send {
+         match resp {
+         Resp::Pong() =>  write_one(io, "".as_bytes()).await,
+         Resp::Data(data) =>  write_one(io, data).await,
+       }
     }
 }
