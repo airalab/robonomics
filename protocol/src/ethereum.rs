02d9c9f5e6807beac7c026af0ac16359ac2d58b1
Denis K <konsistentsi@gmail.com>
2021-10-25 16:16:21 +1000

    ADD: web3

--- protocol/src/ethereum.rs
+++ protocol/src/ethereum.rs
@@ -17,6 +17,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //! Ethereum compatible Robonomics Network types.
 
+use futures::channel::oneshot;
 use jsonrpc_core::types::error::Error;
 use jsonrpc_core::Result;
 use jsonrpc_derive::rpc;
@@ -47,15 +48,28 @@ impl EthApiT for EthApi {
     type Metadata = sc_rpc_api::Metadata;
 
     fn eth_balance(&self, eth_account: String, eth_node: String) -> Result<String> {
-        let account = web3::types::Address::from_str(&eth_account).unwrap();
-        let transport = web3::transports::Http::new(&eth_node).unwrap();
+        let account = match web3::types::Address::from_str(&eth_account) {
+            Ok(account) => account,
+            Err(_) => return Err(Error::internal_error()),
+        };
+        let transport = match web3::transports::Http::new(&eth_node) {
+            Ok(transport) => transport,
+            Err(_) => return Err(Error::internal_error()),
+        };
         let web3 = web3::Web3::new(transport);
+        let (sender, receiver) = oneshot::channel::<_>();
 
         self.task_handle.spawn("eth_balance", async move {
-            let bs = web3.eth().balance(account, None).await;
-            println!("Balance: {:?}", bs);
+            let _ = sender.send(web3.eth().balance(account, None).await);
         });
 
-        Ok("".to_string())
+        futures::executor::block_on(async {
+            match receiver.await {
+                Ok(result) => result
+                    .map(|balance| balance.to_string())
+                    .map_err(|_| Error::internal_error()),
+                Err(_) => Err(Error::internal_error()),
+            }
+        })
     }
 }
