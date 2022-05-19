fe238395153419efa5abc11a1e143533644374c9
Denis K <konsistentsi@gmail.com>
2022-05-19 12:07:08 +1000

    FIX: Pubsub doc

--- protocol/src/pubsub/pubsubapi.rs
+++ protocol/src/pubsub/pubsubapi.rs
@@ -16,6 +16,58 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 //! Robonomics Publisher/Subscriber protocol.
+//!
+//! A basic pubsub client demonstrating libp2p and the gossipsub protocol.
+//!
+//! Using two terminal windows, start two instances.
+//! ```sh
+//! target/debug/robonomics --dev --tmp -l rpc=trace
+//! target/debug/robonomics --dev --tmp --ws-port 9991 -l rpc=trace
+//! ```
+//!
+//! Then using two terminal windows, start two clients.
+//! One of them will send messages, the other one will catch them and print.
+//! You can of course open more terminal windows and add more participants.
+//!
+//! Pubsub subscribe:
+//!
+//! ```{python}
+//! import time
+//! import robonomicsinterface as RI
+//! from robonomicsinterface import PubSub
+//!
+//! def subscription_handler(obj, update_nr, subscription_id):
+//!     print(obj['params']['result'])
+//!     if update_nr >= 2:
+//!         return 0
+//!
+//! interface = RI.RobonomicsInterface(remote_ws="ws://127.0.0.1:9944")
+//! pubsub = PubSub(interface)
+//!
+//! print(pubsub.listen("/ip4/127.0.0.1/tcp/44440"))
+//! time.sleep(2)
+//! print(pubsub.connect("/ip4/127.0.0.1/tcp/44441"))
+//! print(pubsub.subscribe("topic_name", result_handler=subscription_handler))
+//! ```
+//!
+//! Pubsub publish:
+//!
+//! ```{python}
+//! import time
+//! import robonomicsinterface as RI
+//! from robonomicsinterface import PubSub
+//!
+//! interface = RI.RobonomicsInterface(remote_ws="ws://127.0.0.1:9991")
+//! pubsub = PubSub(interface)
+//!
+//! print(pubsub.listen("/ip4/127.0.0.1/tcp/44441"))
+//! time.sleep(2)
+//! print(pubsub.connect("/ip4/127.0.0.1/tcp/44440"))
+//!
+//! for i in range(10):
+//!     time.sleep(2)
+//!     print("publish:", pubsub.publish("topic_name", "message_" + str(time.time())))
+//! ```
 
 use crate::pubsub::{Gossipsub, PubSub};
 use futures::executor;
