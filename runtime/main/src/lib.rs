161efaec80f828df57f3a0bd280b358df22b14f8
Alexander Krupenkin <mail@akru.me>
2022-08-29 16:55:17 +0300

    Bump to polkadot-v0.9.28

--- runtime/main/src/lib.rs
+++ runtime/main/src/lib.rs
@@ -75,7 +75,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("robonomics"),
     impl_name: create_runtime_str!("robonomics-airalab"),
     authoring_version: 1,
-    spec_version: 23,
+    spec_version: 24,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
