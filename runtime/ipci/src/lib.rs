e40768274a4c78d41c67fa240488a438cb8b7d28
Alexander Krupenkin <mail@akru.me>
2022-11-21 16:53:00 +0300

    Bump ipci version

--- runtime/ipci/src/lib.rs
+++ runtime/ipci/src/lib.rs
@@ -75,7 +75,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("ipci"),
     impl_name: create_runtime_str!("ipci-airalab"),
     authoring_version: 1,
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
