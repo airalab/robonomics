1ba399e065d775197bd6648b9b92a7522bd69214
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-12-10 21:44:45 +0600

    Ensure query tool logs don't affect healthcheck

--- scripts/docker/healthcheck.sh
+++ scripts/docker/healthcheck.sh
@@ -3,7 +3,8 @@
 set -e
 
 head () {
-    polkadot-js-api --ws ws://127.0.0.1:9944 query.system.number |\
+    polkadot-js-api --ws ws://127.0.0.1:9944 query.system.number 2>/dev/null |\
+        tail -3 |\
         jq -r .number
 }
 
