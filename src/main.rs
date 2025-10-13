cf22e94c6e7fb204dbfc668943c88d44dd34a220
Alexander Krupenkin <mail@akru.me>
2025-10-13 17:30:11 +0300

    Bump omni node lib version

--- src/main.rs
+++ src/main.rs
@@ -51,9 +51,7 @@ impl CliConfigT for CliConfig {
 fn robonomics_development_config() -> Result<GenericChainSpec, String> {
     let config = GenericChainSpec::builder(
         robonomics_runtime::WASM_BINARY.ok_or("wasm not available")?,
-        Extensions {
-            relay_chain: "westend-local".into(),
-        },
+        Extensions::new("westend-local".into(), 2048),
     )
     .with_name("Robonomics Local Develoment")
     .with_id("robonomics-local-development")
