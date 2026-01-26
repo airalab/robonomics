0c0be915e483dbf500d57e1507651aa50a8ec612
Alexander Krupenkin <mail@akru.me>
2026-01-26 15:15:28 +0300

    Added local-testnet shell (to run zombienet)

--- nix/modules/flake/devshell.nix
+++ nix/modules/flake/devshell.nix
@@ -1,11 +1,9 @@
 {
-  perSystem = { self', pkgs, lib, ... }: {
+  perSystem = { self', config, pkgs, lib, ... }: {
     devShells.default = with pkgs; mkShell.override { stdenv = clangStdenv; } {
         inputsFrom = [ self'.devShells.rust ];
         buildInputs = [
-          openssl taplo
-          polkadot polkadot-parachain
-          zombienet subxt-cli srtool-cli psvm frame-omni-bencher
+          openssl taplo subxt-cli srtool-cli psvm frame-omni-bencher
         ]
         ++ lib.optionals stdenv.hostPlatform.isLinux [ rust-jemalloc-sys ];
 
@@ -14,5 +12,11 @@
         OPENSSL_NO_VENDOR = 1;
         PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
       };
+
+    devShells.local-testnet = with pkgs; mkShell {
+      buildInputs =
+        let robonomics = config.rust-project.crates."robonomics".crane.outputs.drv.crate;
+        in [ polkadot polkadot-parachain zombienet robonomics ];
+    };
   };
 }
