dcaad2ebdd136e2cab1059a47d28280a6e3e06c0
Alexander Krupenkin <mail@akru.me>
2026-08-01 11:33:40 +0300

    Fix nix static cross-compilation

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -2,22 +2,21 @@
 
 let
   # Cross-compilation static targets
-  llvmPkgs = pkgs.pkgsLLVM;
-  x86_64-static = llvmPkgs.pkgsCross.musl64.pkgsStatic;
-  aarch64-static = llvmPkgs.pkgsCross.aarch64-multiplatform-musl.pkgsStatic;
-  riscv64-static = llvmPkgs.pkgsCross.riscv64.pkgsStatic;
+  x86_64-static = pkgs.pkgsCross.musl64.pkgsLLVM;
+  aarch64-static = pkgs.pkgsCross.aarch64-multiplatform-musl.pkgsLLVM;
+  riscv64-static = pkgs.pkgsCross.riscv64.pkgsStatic.pkgsLLVM;
   # Common parameters
   revHash = if (self ? rev) then self.rev else self.dirtyRev;
   protobuf-compiler = "${pkgs.protobuf}/bin/protoc";
 in rec {
   default = robonomics;
 
-  robonomics = llvmPkgs.callPackage ./robonomics { inherit revHash protobuf-compiler; };
+  robonomics = pkgs.callPackage ./robonomics { inherit revHash protobuf-compiler; };
   robonomics-x86_64-static = x86_64-static.callPackage ./robonomics { inherit revHash protobuf-compiler; };
   robonomics-aarch64-static = aarch64-static.callPackage ./robonomics { inherit revHash protobuf-compiler; };
   robonomics-riscv64-static = riscv64-static.callPackage ./robonomics { inherit revHash protobuf-compiler; };
 
-  libcps = llvmPkgs.callPackage ./libcps {};
+  libcps = pkgs.callPackage ./libcps {};
   libcps-x86_64-static = x86_64-static.callPackage ./libcps {};
   libcps-aarch64-static = aarch64-static.callPackage ./libcps {};
   libcps-riscv64-static = riscv64-static.callPackage ./libcps {};
--- nix/pkgs/robonomics/default.nix
+++ nix/pkgs/robonomics/default.nix
@@ -1,45 +1,64 @@
 {
   lib,
-  gcc,
-  cacert,
-  openssl,
-  pkg-config,
+  stdenv,
   protobuf-compiler,
-  rust-jemalloc-sys-unprefixed,
   rustPlatform,
+  llvmPackages,
+  rocksdb,
+  snappy,
   revHash,
   pkgs,
+  isStatic ? stdenv.hostPlatform.isStatic,
 }:
 
-rustPlatform.buildRustPackage {
+let
+  staticSnappy = pkgs.snappy.overrideAttrs (oldAttrs: {
+    cmakeFlags = (oldAttrs.cmakeFlags or []) ++ ["-DBUILD_SHARED_LIBS=OFF"];
+  });
+  sse42Support = stdenv.hostPlatform.sse4_2Support;
+  staticRocksdb = pkgs.rocksdb.overrideAttrs (oldAttrs: {
+    cmakeFlags = [
+      "-DPORTABLE=1"
+      "-DWITH_JEMALLOC=0"
+      "-DWITH_LIBURING=0"
+      "-DWITH_JNI=0"
+      "-DWITH_BENCHMARK_TOOLS=0"
+      "-DWITH_TESTS=0"
+      "-DWITH_TOOLS=0"
+      "-DWITH_CORE_TOOLS=1"
+      "-DWITH_BZ2=0"
+      "-DWITH_LZ4=0"
+      "-DWITH_SNAPPY=1"
+      "-DWITH_ZLIB=0"
+      "-DWITH_ZSTD=0"
+      "-DWITH_GFLAGS=0"
+      "-DUSE_RTTI=1"
+      "-DROCKSDB_BUILD_SHARED=0"
+    ] ++ (lib.optional sse42Support "-DFORCE_SSE42=1") ;
+  }) ;
+in rustPlatform.buildRustPackage {
   pname = "robonomics";
   version = "4.3.0";
 
   cargoLock.lockFile = ../../../Cargo.lock;
   src = lib.cleanSource ../../..;
 
+  nativeBuildInputs = [rustPlatform.bindgenHook]; 
+
+  checkPhase = false;
   buildType = "production";
   buildAndTestSubdir = "bin";
-
-  nativeBuildInputs = [
-    pkg-config
-    gcc.cc.lib
-    rustPlatform.bindgenHook
-  ];
-
-  buildInputs = [
-    openssl
-  ];
-
-  checkInputs = [
-    cacert
-  ];
 
   env = {
     SKIP_WASM_BUILD = 1;
     OPENSSL_NO_VENDOR = 1;
     PROTOC = "${protobuf-compiler}";
     SUBSTRATE_CLI_GIT_COMMIT_HASH = "${builtins.substring 0 7 revHash}";
+    ROCKSDB_LIB_DIR = "${if isStatic then staticRocksdb else rocksdb}/lib";
+    ROCKSDB_STATIC = if isStatic then "1" else "0";
+    SNAPPY_LIB_DIR = "${if isStatic then staticSnappy else snappy}/lib";
+    SNAPPY_STATIC = if isStatic then "1" else "0";
+    RUSTFLAGS = "-Clink-arg=-lc++ -Clink-arg=-lc++abi";
   };
 
   meta = with lib; {
