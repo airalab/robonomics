95448f9af10cd766ab48e3abf4c7d4f0db0df684
Alexander Krupenkin <mail@akru.me>
2026-08-10 04:41:51 +0300

    Add verbose build for static script

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -18,9 +18,9 @@ fi
 echo -e "${GREEN}Building profile: ${PROFILE}${NC}"
 echo ""
 
-docker run -v "${PROJECT_ROOT}":/build -it \
+docker run -v "${PROJECT_ROOT}":/build -i \
     -e SUBSTRATE_CLI_GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
     -e RUSTFLAGS="-Clink-arg=-lzstd" \
     -e SKIP_FRAME_STORAGE_ACCESS_TEST_RUNTIME_WASM_BUILD=1 \
     --rm $(docker build -q "${PROJECT_ROOT}/scripts/docker/builder") \
-    cargo build --profile "${PROFILE}"
+    cargo build -v --profile "${PROFILE}"
